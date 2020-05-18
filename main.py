46e67ee4a34a273073851e461d3712d9838a23fd
ilyailyushko <65430523+ilyailyushko@users.noreply.github.com>
2020-05-18 19:40:58 +0300

    Меню для погода бота

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ bot = telebot.TeleBot(token)
 
 #Инлайн меню_1 уровень
 inline_keyboard= types.InlineKeyboardMarkup(row_width=1)
-btn_1= types.InlineKeyboardButton(text="Резюме", callback_data="1")
-btn_3= types.InlineKeyboardButton(text="Портфолио", callback_data="3")
-btn_4= types.InlineKeyboardButton(text="Контакты", callback_data="4")
-inline_keyboard.add(btn_1,btn_3,btn_4)
+btn_1= types.InlineKeyboardButton(text="Обновить", callback_data="1")
+#btn_3= types.InlineKeyboardButton(text="Погода на завтра", callback_data="3")
+#btn_4= types.InlineKeyboardButton(text="Погода на неделю", callback_data="4")
+inline_keyboard.add(btn_1)
 """#Инлайн меню_2 уровень
 inline_keyboard2= types.InlineKeyboardMarkup(row_width=1)
 btn_1_2= types.InlineKeyboardButton(text="CRM", callback_data="CRM")
@@ -21,14 +21,12 @@ inline_keyboard2.add(btn_1_2,btn_3_2,btn_4_2,btn_5_2,btn_6_2,)"""
 
 @bot.message_handler(commands=['start'])
 def hello_post(message):
-    bot.send_message(message.chat.id, 'Меню превого уровня', reply_markup=inline_keyboard)
+    bot.send_message(message.chat.id, 'Сообщение о погоде', reply_markup=inline_keyboard)
 
 @bot.callback_query_handler (func=lambda call: True)
 def callback_inline(call):
-    if call.data == "3":
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text= "меню2", reply_markup=inline_keyboard2)
-    if call.data == "back":
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Меню превого уровня",reply_markup=inline_keyboard)
+    if call.data == "1":
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text= "Обновленная инфа о погоде", reply_markup=inline_keyboard)
 
 
-bot.polling()
+bot.polling(none_stop=True)
